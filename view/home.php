b5a4db1353a99cfe906b586c1d07134028ab184b
CQD <cqd@hiigara.net>
2022-05-28 19:47:07 +0800

    加上 favicon

--- view/home.php
+++ view/home.php
@@ -2,9 +2,11 @@
 <html lang="zh-tw">
 <head>
 <meta charset="UTF-8">
+<title>噗浪老神機器人網頁版</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<title>噗浪老神機器人網頁版</title>
+<link rel="apple-touch-icon" sizes="180x180" href="/logo.png">
+<link rel="icon" type="image/png" href="/logo.png">
 <style>
 :root{
     background-color: #D58A3B;
