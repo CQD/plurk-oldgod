60a082e76602683d0dff2934fe51442e84d493ef
CQD <cqd@hiigara.net>
2024-12-05 17:02:57 +0800

    logo 換成跟噗浪頭像一樣的新圖

--- view/home.php
+++ view/home.php
@@ -5,8 +5,8 @@
 <title>噗浪老神機器人網頁版</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<link rel="apple-touch-icon" sizes="180x180" href="/logo.png">
-<link rel="icon" type="image/png" href="/logo.png">
+<link rel="apple-touch-icon" sizes="180x180" href="/logo.jpg">
+<link rel="icon" type="image/png" href="/logo.jpg">
 <style>
 :root{
     background-color: #D58A3B;
